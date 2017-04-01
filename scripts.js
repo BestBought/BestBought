87bb7b03fa34b1ac3ef14f85510463f88cb47065
HanWSun <kevinsun872@gmail.com>
2017-04-01 14:52:51 -0700

    Allowed for chatbot string to be converted to product id

--- scripts.js
+++ scripts.js
@@ -1,17 +1,5 @@
 /*jslint devel: true */
-function addTodo() {
-    "use strict";
-    var li = document.createElement("li"), input = document.getElementById("inputTodo");
-    
-    if (input.value.length === 0) {
-        alert("Please type in what you're looking for!");
-    }
-    else {
-        li.innerHTML = input.value;
-        input.value = "";
-        document.getElementById("todos").appendChild(li); 
-    }
-}
+
 function makeCall() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -34,9 +22,65 @@ function makeCall() {
             var bP = obj.badP;
             getPortability(gP, bP);
             
+            //gets top 3 attributes
+            for (var i = 0; i < obj.topAtts.length; i++) {
+                var att = obj.topAtts[i];
+                if (i == 0) {
+                    document.getElementById("k1").innerHTML = att;
+                }
+                if (i == 1) {
+                    document.getElementById("k1").innerHTML = att;
+                }
+                if (i == 2) {
+                    document.getElementById("k1").innerHTML = att;
+                }
+                
+            }
+            //gets top 5 positive reviews and adds them to list
+            for (var i = 0; i < obj.goodComments.length; i++) {
+                var goodCom = obj.goodComments[i];
+                if (i == 0) {
+                    document.getElementById("c1r1").innerHTML = goodCom;
+                }
+                if (i == 1) {
+                    document.getElementById("c1r2").innerHTML = goodCom;
+                }
+                if (i == 2) {
+                    document.getElementById("c1r3").innerHTML = goodCom;
+                }
+                if (i == 3) {
+                    document.getElementById("c1r4").innerHTML = goodCom;
+                }
+                if (i == 4) {
+                    document.getElementById("c1r5").innerHTML = goodCom;
+                }
+                
+            }
+            //gets top 5 negative reviews and adds them to list
+            for (var i = 0; i < obj.badComments.length; i++) {
+                var badCom = obj.badComments[i];
+                if (i == 0) {
+                    document.getElementById("c2r1").innerHTML = badCom;
+                }
+                if (i == 1) {
+                    document.getElementById("c2r2").innerHTML = badCom;
+                }
+                if (i == 2) {
+                    document.getElementById("c2r3").innerHTML = badCom;
+                }
+                if (i == 3) {
+                    document.getElementById("c2r4").innerHTML = badCom;
+                }
+                if (i == 4) {
+                    document.getElementById("c2r5").innerHTML = badCom;
+                }
+                
+            }
+            
+            
         }
     };
-    xhttp.open("GET", "http://52.160.109.98:8080/help", true);
+    xhttp.open("GET", "http://52.160.109.98:8080/product/1045309", true);
     xhttp.send();
 }
 
@@ -60,11 +104,89 @@ function getReliability(g, b) {
 function getBattery (g, b) {
     document.getElementById("BatteryS").innerHTML = g + " : " + b;
 }
+
 function getPortability (g, b) {
     document.getElementById("PortabilityS").innerHTML = g + " : " + b;
 }
 
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
+var ajax = {};
+ajax.x = function () {
+    if (typeof XMLHttpRequest !== 'undefined') {
+        return new XMLHttpRequest();
+    }
+    var versions = [
+        "MSXML2.XmlHttp.6.0",
+        "MSXML2.XmlHttp.5.0",
+        "MSXML2.XmlHttp.4.0",
+        "MSXML2.XmlHttp.3.0",
+        "MSXML2.XmlHttp.2.0",
+        "Microsoft.XmlHttp"
+    ];
+
+    var xhr;
+    for (var i = 0; i < versions.length; i++) {
+        try {
+            xhr = new ActiveXObject(versions[i]);
+            break;
+        } catch (e) {
+        }
+    }
+    return xhr;
+};
+
+ajax.send = function (url, callback, method, data, async) {
+    if (async === undefined) {
+        async = true;
+    }
+    var x = ajax.x();
+    x.open(method, url, async);
+    x.onreadystatechange = function () {
+        if (x.readyState == 4) {
+            callback(x.responseText)
+        }
+    };
+    if (method == 'POST') {
+        x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    }
+    x.send(data)
+};
+
+ajax.get = function (url, data, callback, async) {
+    var query = [];
+    for (var key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+    }
+    ajax.send(url + (query.length ? '?' + query.join('&') : ''), callback, 'GET', null, async)
+};
+
+ajax.post = function (url, data, callback, async) {
+    var query = [];
+    for (var key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+    }
+    ajax.send(url, callback, 'POST', query.join('&'), async)
+};
+
+var productTitle = getParameterByName('productitle');
+
+ajax.get("https://msi.bbycastatic.ca/mobile-si/si/v3/products/search", {"query" : productTitle}, getProductID, true);
+
+function getProductID(data) {
+    var productId = JSON.parse(data).searchApi.documents[0].skuId;
+    alert(productId);
+}
 /*
 var list = document.querySelectorAll('td.cicon');
 list.forEach.addEventListener('click', function(ev) {
