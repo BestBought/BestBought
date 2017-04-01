4f80a3fae32dafd001cab7aab31ff3fb2ea72721
HanWSun <kevinsun872@gmail.com>
2017-04-01 13:08:20 -0700

    Added functionality for parsing json

--- scripts.js
+++ scripts.js
@@ -18,23 +18,65 @@ function makeCall() {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
             console.log(obj);
-            document.getElementById("demo").innerHTML = obj;
+            
+            var sent = obj.goodScore;
+            getSentimant(sent);
+            
+            var gR = obj.goodR;
+            var bR = obj.badR;
+            getReliability(gR, bR);
+            
+            var gB = obj.goodB;
+            var bB = obj.badB;
+            getBattery(gB, bB);
+            
+            var gP = obj.goodP;
+            var bP = obj.badP;
+            getPortability(gP, bP);
+            
         }
     };
     xhttp.open("GET", "http://52.160.109.98:8080/help", true);
     xhttp.send();
 }
 
-var go = document.getElementById("addTodo");
-var txt = document.getElementById("inputTodo");
-
-txt.addEventListener("keypress", function (event) {
-    "use strict";
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        go.click();
+function getSentimant(score) {
+    if (score <= .40) {
+        document.getElementById("OverallS").innerHTML =
+        "Critical";
     }
-});
+    if (score >= .70) {
+        document.getElementById("OverallS").innerHTML = "Positive";
+    }
+    else {
+        document.getElementById("OverallS").innerHTML = "Mixed";
+    }
+}
+
+function getReliability(g, b) {
+    document.getElementById("ReliabilityS").innerHTML = g + " : " + b;
+}
+
+function getBattery (g, b) {
+    document.getElementById("BatteryS").innerHTML = g + " : " + b;
+}
+function getPortability (g, b) {
+    document.getElementById("PortabilityS").innerHTML = g + " : " + b;
+}
+
+
+/*
+var list = document.querySelectorAll('td.cicon');
+list.forEach.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'TD') {
+    ev.target.classList.toggle('negative');
+    
+  }
+}, false);
+
+*/
+                         
+
 
                                                                              
 
